4c17508c6cd2ee9de765afa31be1e7d549a10626
JWizerd <jwodke@outlook.com>
2017-07-25 20:02:53 -0600

    toggle post content with js and simple styling for zen habits blog post area

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,12 @@
-function littleLazyLoader(postClass) {
-  var $post       = $("." + postClass);
-      postContent = $post.html();
+// TOGGLE POST CONTENT
+function togglePostContent() {
+  $post = $('.post');
+  $post.after('<button class="btn btn-default btn-article">Read Article</button>');
+  $post.addClass('hide-post-content');
 
-  function postExcerpt() {
-    var half_length = Math.round($post.html().length / 10)
-        $excerpt    = $post.html().slice(0, half_length)
-        $excerpt    += "<div><a class='read-more text-center btn btn-success'>Read More</a></div>";
-
-    $post.html($excerpt)
-  }
-
-  function showMoreContent() {
-    $('.read-more').click(function(e){
-      e.preventDefault();
-      $post.html(postContent);
-    });
-  }
-
-  console.log($post);
-  postExcerpt();
-  showMoreContent()
+  $('.btn-article').on('click', function(){
+    $post.toggleClass('hide-post-content');
+  }); 
 }
 
-littleLazyLoader('post');
+togglePostContent();
--- zenhabits.php
+++ zenhabits.php
@@ -1,3 +1,3 @@
-<h1 class="text-center">Zen Habits</h1>
+<h1 class="text-center"><strong class="text-success">Zen Habits</strong> : <em>Daily Dose</em></h1>
 <hr>
 <? require('controllers/zenhabits.php'); ?>
