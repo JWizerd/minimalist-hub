7dd793ef1173294d5cbf4eefa9b56b741413b6c5
JWizerd <jwodke@outlook.com>
2017-07-25 21:06:33 -0600

    further styling and adding fonts

--- zenhabits.php
+++ zenhabits.php
@@ -1,3 +1,6 @@
-<h1 class="text-center"><strong class="text-success">Zen Habits</strong> : <em>Daily Dose</em></h1>
-<hr>
-<? require('controllers/zenhabits.php'); ?>
+<div class="zen-habits">
+  <h1 class="text-center"><strong class="text-success">Zen Habits</strong> : <em>Daily Dose</em></h1>
+  <hr>
+  <? require('controllers/zenhabits.php'); ?>  
+</div>
+
