463cbaf8ffd1ecbf432cc1d8d94dd0cfc9219fa3
JWizerd <jwodke@outlook.com>
2017-08-02 19:19:57 -0600

    refactored rss_parser method in rss_feed class

--- controllers/webscraper.php
+++ controllers/webscraper.php
@@ -1,37 +1,46 @@
 <?
-  class RSS_Feed {
-    public $url;
-    public $rss;
-    public $feed = [];
-    public $post_obj;
+class RSS_Feed {
+  public $url;
+  public $rss;
+  public $feed = [];
+  public $post_obj;
 
-    public function __construct($url) {
-      $this->url = $url;
-      $this->rss_parser($this->url);
-      $this->get_latest_post($this->url);
-    }
+  public function __construct($url) {
+    $this->url = $url;
+    $this->rss_parser($this->url);
+    $this->get_latest_post($this->url);
+  }
 
-    public function get_latest_post($url) {
-     if (!empty($this->feed)) {
-        $this->post_obj = $this->feed[0];
-      } else {
-       false;
-      } 
-    }
+  public function get_latest_post($url) {
+   if (!empty($this->feed)) {
+      $this->post_obj = $this->feed[0];
+    } else {
+     false;
+    } 
+  }
 
-    public function rss_parser($url) {
-      // using DOMObject to parse through XML encoded items
-      $dom_object = new DOMDocument();
-      @$dom_object->load($url);
-      if (!empty($dom_object->getElementsByTagName('item'))) {
-        foreach ($dom_object->getElementsByTagName('item') as $node) {
-          $item = [
-                  'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
-                  'content' => $node->getElementsByTagName('encoded')->item(0)->nodeValue
-                  ];
-          array_push($this->feed, $item);
-        }
+  public function rss_parser($url) {
+    // using DOMObject to parse through XML encoded items
+    $dom_object = new DOMDocument();
+    @$dom_object->load($url);
+
+    if ($this->proper_rss($dom_object)) {
+
+      foreach ($dom_object->getElementsByTagName('item') as $node) {
+        
+        $item = [
+                'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+                'content' => $node->getElementsByTagName('encoded')->item(0)->nodeValue
+                ];
+        array_push($this->feed, $item);
+
       }
+
     }
   }
+
+  public function proper_rss($dom_object) {
+    return !empty($dom_object->getElementsByTagName('item'));
+  }
+}
 
